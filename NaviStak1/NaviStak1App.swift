e25f5821dc60d9abf62c27baea0141b1cdc0c923
moritamu <moris@r.sannet.ne.jp>
2022-10-30 09:28:54 +0900

    Initial Commit

---
+++ NaviStak1/NaviStak1App.swift
@@ -0,0 +1,17 @@
+//
+//  NaviStak1App.swift
+//  NaviStak1
+//
+//  Created by 森田晋 on 2022/10/30.
+//
+
+import SwiftUI
+
+@main
+struct NaviStak1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
